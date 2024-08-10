dbb82e733816d4bb11b393b81230fede43b696da
James Brundage <@github.com>
2024-08-10 12:09:51 -0700

    fix: Dice test fixes ( Fixes #33 )

--- Dice.tests.ps1
+++ Dice.tests.ps1
@@ -6,7 +6,10 @@ describe Dice {
     it 'Is takes no time for a computer to flip a coin 10kb times.' {
         Measure-Command { 
              Read-Dice -Sides 2 -RollCount 10kb
-        } | Select-Object -ExpandProperty TotalSeconds | Should -BeLessThan 2
+        } | 
+        Select-Object -ExpandProperty TotalSeconds | 
+        Should -BeLessThan 5 # It should be a lot less than 5 seconds,
+        # but we'll give it a little wiggle room.
     }
 
     it 'Can measure the variance correctly' {
@@ -15,7 +18,8 @@ describe Dice {
     }
 
     it 'Can prove rolling 20 is lucky' {
-        Read-Dice -Sides 20 -RollCount 10kb | 
+        Read-Dice -Sides 20 -RollCount 10kb |
+            Select-Object -ExpandProperty Rolls |
             Where-Object Roll -EQ 20 |
             Select-Object -ExpandProperty StandardUnit |
             Should -BeGreaterThan 1.5
